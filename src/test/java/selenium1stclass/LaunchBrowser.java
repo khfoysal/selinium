528f09829696dbcbd7c7060bec90a3ba290c04bd
khfoysal <khandakerfoysal518@gmail.com>
2025-01-22 14:32:52 +0600

    first commit

---
+++ src/test/java/selenium1stclass/LaunchBrowser.java
@@ -0,0 +1,14 @@
+package selenium1stclass;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LaunchBrowser {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("https://mvnrepository.com/");
+        driver.quit();
+
+    }
+}
